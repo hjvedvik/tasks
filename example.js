ea19651f1137c74d2e9ca1236e60598d81b023ef
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-08-07 12:10:40 +0200

    feat(task): option to skip task

--- example.js
+++ example.js
@@ -6,10 +6,15 @@ const list = new Tasks([
     task: () => heavyTask()
   },
   {
+    title: 'Allways skip this task',
+    skip: () => true,
+    task: () => {}
+  },
+  {
     title: 'Progress bar',
     task: (_, task) => {
       for (let i = 0; i <= 5; i++) {
-        heavyTask()
+        heavyTask(5)
         task.setProgress(i, 5)
       }
     }
@@ -18,19 +23,19 @@ const list = new Tasks([
     title: 'Status and summary',
     task: (_, task) => {
       for (let i = 1; i < 5; i++) {
-        task.setStatus(`Working with ${i} of 5`)
+        task.setStatus(`I'm now working with ${i} of 5`)
         heavyTask(4)
       }
-      task.setSummary('All tasks completed successfully')
+      task.setSummary('I finished them all!')
     }
   },
   {
     title: 'Subtasks',
     task: () => new Tasks([
       {
-        title: 'With progressbar',
+        title: 'Subtask can also have progress bar',
         task: (_, task) => {
-          const length = 1000000
+          const length = 100000000
           for (let i = 0; i <= length; i++) {
             task.setProgress(i, length)
           }
@@ -38,25 +43,7 @@ const list = new Tasks([
       },
       {
         title: 'Another subtask',
-        task: () => new Tasks([
-          {
-            title: 'With progressbar',
-            task: (_, task) => {
-              for (let i = 0; i <= 10; i++) {
-                heavyTask()
-                task.setProgress(i, 10)
-              }
-            }
-          },
-          {
-            title: 'Another subtask',
-            task: () => {
-              heavyTask()
-            }
-          }
-        ], {
-          showTasks: false
-        })
+        task: () => heavyTask()
       }
     ])
   },
